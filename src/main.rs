cda773f0213e078bd4c57094bcf9e37a211e4a53
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-07-16 21:37:12 -0600

    feat: añadir estructura y código modificado para crear lineas con Bresenham

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod framebuffer;
+mod line;
+mod bmp;
+
+use crate::bmp::WriteBmp;
+use crate::framebuffer::Framebuffer;
+use crate::line::draw_line;
+
+fn main() {
+    let mut framebuffer = Framebuffer::new(800, 600);
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    draw_line(100, 300, 700, 300, &mut framebuffer);
+    draw_line(400, 100, 400, 500, &mut framebuffer);
+    framebuffer.point(0, 0);
+
+    let _ = framebuffer.render_buffer("out.bmp");
+
+    println!("Framebuffer rendered to out.bmp");
+}
